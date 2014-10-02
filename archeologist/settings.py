3e9a9b8fb526e9431e4bc34f07b2cbac2c6a3085
Jason Thorne <jrthorne@randalds.com.au>
2014-10-02 19:48:02 +1000

    With the move to the new server, I now need to use collect static. This means that static files are now in the assets directory, and they are served from here when running the server locally

--- archeologist/settings.py
+++ archeologist/settings.py
@@ -122,7 +122,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE			= True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT 			= "" #os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT 			= os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
-	'/www/archeologistRoot/archeologist/static/',
+	'/www/archeologistRoot/assets/',
 )
 
 # List of finder classes that know how to find static files in
--- wsgi/archeologist.wsgi
+++ wsgi/archeologist.wsgi
@@ -1,5 +1,10 @@
 import os
 import sys
+import site
+
+# Add the site-packages of the chosen virualenv to work with
+site.addsitedir('/home/jason/virtualenvs/archeologist/lib/python2.7/site-packages')
+
 # need both the surveys site directory
 myPath      = '/www/archeologistRoot'
 if myPath not in sys.path:
@@ -13,6 +18,10 @@ if myPath not in sys.path:
 # end if
 
 os.environ['DJANGO_SETTINGS_MODULE']    = 'archeologist.settings'
+
+# activate your virtual env
+activate_env=os.path.expanduser("/home/jason/virtualenvs/archeologist/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
