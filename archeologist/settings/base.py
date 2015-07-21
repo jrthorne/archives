21e4f5317917263ddcfec9cd4c20727d26b75fe9
jrthorne <jthorne@magiclamp.com.au>
2015-07-22 07:26:44 +1000

    Moved the database settings to local.py

--- archeologist/settings/base.py
+++ archeologist/settings/base.py
@@ -56,7 +56,8 @@ ADMINS                     = (
 
 MANAGERS                 = ADMINS
 
-DATABASES                 = {
+# moved to local.py
+"""DATABASES                 = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'magiclamp$archives',                      # Or path to database file if using sqlite3.
@@ -65,7 +66,8 @@ DATABASES                 = {
         'HOST': 'magiclamp.mysql.pythonanywhere-services.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
-}
+}"""
+
 
 # where do we go to login?
 LOGIN_URL               = '/archives/login'
