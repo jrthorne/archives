fba8f268921e596ed958c609952f02fab61e5990
jrthorne <jthorne@magiclamp.com.au>
2015-07-22 10:32:52 +1000

    Static files were in the wrong place.

--- archeologist/settings/base.py
+++ archeologist/settings/base.py
@@ -115,7 +115,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE            = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT             = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT             = os.path.join(PROJECT_ROOT, '../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
