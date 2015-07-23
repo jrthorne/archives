3a065ef647652dce00109abf2bb8aaa97aa961b5
jrthorne <jthorne@magiclamp.com.au>
2015-07-24 07:13:59 +1000

    Converted tabs to spaces as per PEP8

--- survey/views.py
+++ survey/views.py
@@ -19,31 +19,31 @@ Field.default_error_messages = {
 
 
 def Index(request):
-	BING_KEY        = settings.BING_KEY
-	surveyList      = Survey.objects.all()
-	
-	return render_to_response('survey/index.html',locals())
+    BING_KEY        = settings.BING_KEY
+    surveyList      = Survey.objects.all()
+    
+    return render_to_response('survey/index.html',locals())
 
 
 def SurveyDetail(request, id):
-	survey = Survey.objects.get(id=id)
-	category_items = Category.objects.filter(survey=survey)
-	#categories = [c.name for c in category_items]
-	if request.method == 'POST':
-		form = ResponseForm(request.POST, survey=survey)
-		if form.is_valid():
-			response = form.save()
-			if int(id)==1 :
-				return HttpResponseRedirect("/survey/confirmPre/"+ response.registerNumber )
-			if int(id)==2 :
-				return HttpResponseRedirect("/survey/confirmPost/"+ response.registerNumber )
-			else:
-				return HttpResponseRedirect('/survey/')
-	else:
-		form = ResponseForm(survey=survey)
-		print format
-		
-	return render(request, 'survey/survey.html', {'response_form': form, 'survey': survey, 'category_items': category_items})
+    survey = Survey.objects.get(id=id)
+    category_items = Category.objects.filter(survey=survey)
+    #categories = [c.name for c in category_items]
+    if request.method == 'POST':
+        form = ResponseForm(request.POST, survey=survey)
+        if form.is_valid():
+            response = form.save()
+            if int(id)==1 :
+                return HttpResponseRedirect("/survey/confirmPre/"+ response.registerNumber )
+            if int(id)==2 :
+                return HttpResponseRedirect("/survey/confirmPost/"+ response.registerNumber )
+            else:
+                return HttpResponseRedirect('/survey/')
+    else:
+        form = ResponseForm(survey=survey)
+        print format
+        
+    return render(request, 'survey/survey.html', {'response_form': form, 'survey': survey, 'category_items': category_items})
 
 def ConfirmPre(request):
     return render(request, 'survey/confirmPre.html')
